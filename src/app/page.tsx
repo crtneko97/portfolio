63a499a65d83f3255cc4b70fad19090252e7899c
crtneko97 <simon_kern@hotmail.com>
2024-07-31 17:04:13 +0200

    desktop multiple cards etc

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,13 @@
 import React from 'react'
 import Introduction from './components/introduction/Introduction';
 
+
 const Home = () => {
   return (
     <main>
     <Introduction name={'Simon Kern'} location={'Sweden/Stockholm'} github={'https://github.com/crtneko97'} linkedin={'https://www.linkedin.com/in/simonkern97/'} />
+    <Introduction name={'Simon Kern'} location={'Sweden/Stockholm'} github={'https://github.com/crtneko97'} linkedin={'https://www.linkedin.com/in/simonkern97/'} />
+    
     </main>
   );
 }
